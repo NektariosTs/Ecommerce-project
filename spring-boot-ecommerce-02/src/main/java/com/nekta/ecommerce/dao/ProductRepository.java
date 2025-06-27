5538d2d9ef2f22f1abd8216e19824418533a37c0
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-27 20:20:51 +0300

    Back end/Spring boot

---
+++ spring-boot-ecommerce-02/src/main/java/com/nekta/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.nekta.ecommerce.dao;
+
+import com.nekta.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
